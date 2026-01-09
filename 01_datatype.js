0421398a7630d6a3aaf577958120f123eef93233
himanshu prajapat <salasarhimanshu6@gmail.com>
2026-01-09 07:42:55 +0530

    Add files via upload

---
+++ 01_datatype.js
@@ -0,0 +1,25 @@
+"use strict"; //treat all js code as newer version
+
+alert("hello world");// we are using node.js, not browser
+console.log("hello world"); //code redability shoud be high
+
+let name = "himanshu";
+let age = 19;
+let isLoggedIN = false;
+
+
+//number => 2 to power 53
+//bigint => larger than 2 to power 53
+let bigNumber = 1234567890123456789012345678901234567890n;
+
+// String =>"hello" 'hello' `hello`
+let string1 = "hello";
+
+// Boolean => true/false
+let isLoggedIn = false;
+
+// null => standalone value
+let empty = null;
+
+// undefined => value not assigned
+let undef = undefined;
