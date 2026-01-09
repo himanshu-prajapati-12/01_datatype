3173a13055d3e3c197a0fc1498e346beb3089564
himanshu prajapat <salasarhimanshu6@gmail.com>
2026-01-09 10:59:05 +0530

    Add files via upload

---
+++ 03_operations.js
@@ -0,0 +1,21 @@
+let number = 3;
+let negative = -number;
+// console.log(negative);
+
+
+let str1 = "hellow";
+let str2 = " himanhsu"
+let str3 = str1 + str2
+
+// console.log("1" + 2) //12
+// console.log(1 + "20"); //12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); //32
+
+console.log(+true);
+console.log(+false);
+console.log(+"")
+
+// ++i =>
+// i++ =>
+    In pre-increment (++x), the value is increased before it is used, while in post-increment (x++), the value is used first and then increased.
